e030279b6c2021b61dd9024ad088b19a36f44c8b
MARCELO FRANCISCONI LUBANCO THOMÉ <lubanco@edu.unifil.br>
2018-11-02 03:53:01 -0300

    Primeira aplicação com Vue.js

---
+++ aplicativo.js
@@ -0,0 +1,25 @@
+var app = new Vue({
+    el: '#app',
+    data: {
+        message: 'Hello Vue!'
+    },
+
+    // listadado = ["Ross Lynch - Locked Out Of Heaven",
+    //   "Charlie Puth - Marvin Gaye ft. Meghan Trainor [Official Video]"],
+
+    lista : [
+        {
+            id: 1, video: "Ross Lynch - Locked Out Of Heaven"
+        },
+        {
+            id: 2, video: "Charlie Puth - Marvin Gaye ft. Meghan Trainor [Official Video]"
+        },
+        {
+            id: 3, video: "Charlie Puth - We Don't Talk Anymore (feat. Selena Gomez) [Official Video"
+        },
+    ]
+
+
+});
+
+
